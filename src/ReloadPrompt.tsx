8b5dee4847bbaf8ae8d7d9877dab9f89f1bd336e
danidn93 <dario93jossi@hotmail.com>
2026-01-21 08:37:15 -0500

    Cambio en ReloadPrompt

--- src/ReloadPrompt.tsx
+++ src/ReloadPrompt.tsx
@@ -1,30 +1,19 @@
-// src/ReloadPrompt.tsx
 import { useRegisterSW } from 'virtual:pwa-register/react'
 import { useEffect } from 'react'
 
 function ReloadPrompt() {
   const {
-    offlineReady: [offlineReady, setOfflineReady],
-    needRefresh: [needRefresh, setNeedRefresh],
+    needRefresh: [needRefresh],
     updateServiceWorker,
-  } = useRegisterSW({
-    onRegistered(r) {
-      console.log('SW Registered')
-    },
-    onRegisterError(error) {
-      console.error('SW registration error', error)
-    },
-  })
+  } = useRegisterSW()
 
   useEffect(() => {
     if (needRefresh) {
-      // Esta línea es la que hace la magia:
-      // Si detecta una nueva versión, actualiza el SW y recarga la página
       updateServiceWorker(true)
     }
   }, [needRefresh, updateServiceWorker])
 
-  return null // No necesita renderizar nada visual si quieres que sea automático
+  return null
 }
 
 export default ReloadPrompt
